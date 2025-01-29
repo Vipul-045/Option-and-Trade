8981cc42ca8468433f716a88335b4c9c628e1779
Vipul Yadav <vipulyadav0479@gmail.com>
2025-01-29 22:52:42 +0530

    About us

--- src/pages/Aboutus.tsx
+++ src/pages/Aboutus.tsx
@@ -2,7 +2,17 @@
 
 function Aboutus() {
   return (
-    <div>Hello from Aboutus</div>
+    <div>
+      <div className="justify-center text-center">
+        <p className="text-4xl text-center mt-40">
+          We Are Experts In Banknifty And Nifty Option Advisory
+          We are the most reliable stock market advisory services; in essence, we are a research company for technology that supports trading.
+        </p>
+        <p className="  max-w-4xl text-lg">
+          Our expert advisers provide trading recommendations for indexes like Banknifty and Nifty; we are experts in Banknifty and Nifty Option Advisory; we have assisted more than 9,000 satisfied clients over the course of our more than 12 years in the stock market.
+        </p>
+      </div>
+    </div>
   )
 }
 
